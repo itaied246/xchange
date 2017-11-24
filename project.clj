16b5c8da14260814813bbe93e5205c51cf2588a8
itaied <itaied246@gmail.com>
2017-11-24 12:48:21 +0200

    add migratus and basic migration

--- project.clj
+++ project.clj
@@ -7,8 +7,13 @@
                  [environ "1.1.0"]
                  [bouncer "1.0.1"]
                  [org.clojure/java.jdbc "0.7.3"]
-                 [org.postgresql/postgresql "42.1.4"]]
-  :plugins [[lein-environ "1.1.0"]]
+                 [org.postgresql/postgresql "42.1.4"]
+                 [migratus "1.0.1"]]
+  :plugins [[lein-environ "1.1.0"]
+            [migratus-lein "0.5.3"]]
   :main ^:skip-aot xchange.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}}
+  :migratus {:store         :database
+             :migration-dir "migrations"
+             :db            "postgresql://postgres:pass@localhost/xchange"})
