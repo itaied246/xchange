1a62be4277b7ed256e8eb8a8cbde0e86c7f01a7b
itaied <itaied246@gmail.com>
2017-12-14 20:13:36 +0200

    add users query and modify tests resolvers

--- test/xchange/test_utils.clj
+++ test/xchange/test_utils.clj
@@ -2,9 +2,9 @@
   (:require [user]))
 
 (def stub-context
-  {:query/user (fn [] {:id "1"})})
+  {:query/user  (fn [& _] {:id "1"})
+   :query/users (fn [& _] '({:id "1"}))})
 
 (defn q
   [query-string]
   (user/q query-string stub-context))
-
