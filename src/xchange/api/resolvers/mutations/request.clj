5bb9c32478323a5f2fa2abc77375c6717c757b54
itaied <itaied246@gmail.com>
2018-01-07 20:28:37 +0200

    add request mutation tests

--- src/xchange/api/resolvers/mutations/request.clj
+++ src/xchange/api/resolvers/mutations/request.clj
@@ -3,13 +3,22 @@
             [xchange.api.schema-validations :as sv]))
 
 (defn create-request
-  [context args value])
+  [context args value]
+  (let [[res err-msg] (sv/validate-args sv/request-input-schema args)]
+    (if-not res
+      (resolve-as nil err-msg))))
 
 (defn update-request
-  [context args value])
+  [context args value]
+  (let [[res err-msg] (sv/validate-args sv/request-input-schema args)]
+    (if-not res
+      (resolve-as nil err-msg))))
 
 (defn remove-request
   [context args value])
 
 (defn add-request-comment
-  [context args value])
+  [context args value]
+  (let [[res err-msg] (sv/validate-args sv/comment-input-schema args)]
+    (if-not res
+      (resolve-as nil err-msg))))
