36d0478c202114d6faa0d7f0fd22cc45abe990da
itaied <itaied246@gmail.com>
2018-01-20 19:33:30 +0200

    add user query tests

--- test/xchange/api/queries/user_test.clj
+++ test/xchange/api/queries/user_test.clj
@@ -7,10 +7,18 @@
   (testing "id is required"
     (missing-args? '(:id) "{ user { id } }"))
 
-  (testing "query by id"
-    (valid? "{ user (id: \"1\") { id } }"))
+  (testing "valid query schema"
+    (valid? "{ user (id: \"1\")
+                    { id
+                      created_at
+                      name
+                      email
+                      phone
+                      offers { id }
+                      requests { id }
+                      comments { id } } }")))
 
-  (testing "query all users"
-    (valid? "{ users { id } }"))
+(deftest users
 
-  )
+  (testing "valid query schema"
+    (valid? "{ users { id } }")))
