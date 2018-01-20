49cd4b2c4bd9e9dedef73dba53a87e0dfdbf4817
itaied <itaied246@gmail.com>
2018-01-20 19:29:38 +0200

    add create_user tests

--- test/xchange/api/mutations/user_test.clj
+++ test/xchange/api/mutations/user_test.clj
@@ -18,3 +18,14 @@
   (testing "name max length is 50"
     (let [exceed-name (clojure.string/join (repeat 51 "q"))]
       (is (not (s/valid? :xchange.api.resolvers.mutations.user/name exceed-name))))))
+
+(deftest create-user
+
+  (testing "name is required"
+    (missing-args? '(:name) "mutation { create_user { id } }"))
+
+  (testing "valid mutation schema"
+    (valid? "mutation { create_user (name: \"Itai Edri\"
+                                     email: \"itai@edri.com\"
+                                     phone: \"1234567890\")
+                                    { id } }")))
