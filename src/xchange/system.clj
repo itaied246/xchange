794a3d5ac81fbf2d2e4955de28201def01790b46
itaied <itaied246@gmail.com>
2018-01-22 20:55:58 +0200

    create db component

--- src/xchange/system.clj
+++ src/xchange/system.clj
@@ -2,13 +2,15 @@
   (:require
     [com.stuartsierra.component :as component]
     [xchange.api.schema :as schema]
-    [xchange.server.server :as server]))
+    [xchange.server.server :as server]
+    [xchange.data.db :as db]))
 
 (defn new-system
   [config]
-  (let [{:keys [port]} config]
+  (let [{:keys [port db-url]} config]
     (component/system-map
       :schema (schema/new-schema)
+      :db (db/new-db db-url)
       :server (component/using
                 (server/new-server port)
-                [:schema]))))
+                [:schema :db]))))
